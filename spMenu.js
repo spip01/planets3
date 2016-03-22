096e0346e409df86ddf54d060d610f941eea0f3e
spip01 <sp@farpoint.us>
2016-03-22 13:10:01 -0500

    formatting

--- spMenu.js
+++ spMenu.js
@@ -16,166 +16,166 @@
 // ==/UserScript==
 
 function wrapper() {
-	var showReady = false;
-	var showMenu = false;
-	var clearList=[];
+  var showReady = false;
+  var showMenu = false;
+  var clearList = [];
 
-	function spTools() {}
-	spTools.prototype = {
+  function spTools() {
+  }
+  spTools.prototype = {
 
-		// this handles all of the menu controls. The only thing child scripts
-		// need to call is vgapMap.prototype.addTool(text, class, function)
-			
-		loadControls : function() {
-			// debugger;
-			var a = "<div id='spControls'><div id='spToolsMenu'>sp Tools</div><ul id='spTools'></ul></div>";
-			$(a).appendTo("#PlanetsContainer");
+    // this handles all of the menu controls. The only thing child scripts
+    // need to call is vgapMap.prototype.addTool(text, class, function)
 
-			var s = {
-				"top" : "0px",
-				"display" : "block",
-				"position" : "absolute",
-				"right" : "260px",
-				"color" : "rgb(255,255,255)",
-				"z-index" : "0",
-				"width" : "90px",
-				"background-color" : "rgb(51,51,51)",
-				"border-bottom-left-radius" : "10px",
-				"border-bottom-right-radius" : "10px",
-				"text-align" : "center",
-			};
-			$("#spControls").css(s);
+    loadControls : function() {
+      // debugger;
+      var a = "<div id='spControls'><div id='spToolsMenu'>sp Tools</div><ul id='spTools'></ul></div>";
+      $(a).appendTo("#PlanetsContainer");
 
-			s = {
-				"display" : "block",
-				"width" : "80px",
-				"text-align" : "center",
-				"font-size" : "11px",
-				"list-style" : "none",
-				"line-height" : "20px",
-				"padding" : "0",
-				"margin" : "5px",
-				"cursor" : "default",
-			};
-			$("#spTools").css(s);
+      var s = {
+	"top" : "0px",
+	"display" : "block",
+	"position" : "absolute",
+	"right" : "260px",
+	"color" : "rgb(255,255,255)",
+	"z-index" : "0",
+	"width" : "90px",
+	"background-color" : "rgb(51,51,51)",
+	"border-bottom-left-radius" : "10px",
+	"border-bottom-right-radius" : "10px",
+	"text-align" : "center",
+      };
+      $("#spControls").css(s);
 
-			s = {
-				"display" : "block",
-				"padding" : "0",
-				"line-height" : " 38px",
-				"width" : "70px",
-				"background-color" : "rgb(51,51,51)",
-				"cursor" : "default",
-			};
-			$("#spToolsMenu").css(s);
+      s = {
+	"display" : "block",
+	"width" : "80px",
+	"text-align" : "center",
+	"font-size" : "11px",
+	"list-style" : "none",
+	"line-height" : "20px",
+	"padding" : "0",
+	"margin" : "5px",
+	"cursor" : "default",
+      };
+      $("#spTools").css(s);
 
-			s = {
-				"border-bottom-style" : "groove",
-				"line-height" : "24px",
-			};
-			$("#spTools li").css(s);
+      s = {
+	"display" : "block",
+	"padding" : "0",
+	"line-height" : " 38px",
+	"width" : "70px",
+	"background-color" : "rgb(51,51,51)",
+	"cursor" : "default",
+      };
+      $("#spToolsMenu").css(s);
 
-			this.clearControls();
+      s = {
+	"border-bottom-style" : "groove",
+	"line-height" : "24px",
+      };
+      $("#spTools li").css(s);
 
-			$("#spControls").mouseenter(function() {
-				$("#spControls").css("cursor:pointer");
-			});
+      this.clearControls();
 
-			$("#spControls").mouseleave(function() {
-				$("#spControls").css("cursor:default");
-			});
+      $("#spControls").mouseenter(function() {
+	$("#spControls").css("cursor:pointer");
+      });
 
-			// text, class, function
-			vgapMap.prototype = {
-				spMenuItem : function(c, a, b) {
-					spTools.prototype.addTool(c, a, b);
-				}
-			};
+      $("#spControls").mouseleave(function() {
+	$("#spControls").css("cursor:default");
+      });
 
-			this.addTool("Clear All", "clearAll", function() {
-				for (i = 0; i < clearList.length; ++i)
-					clearList[i]();
-				spTools.prototype.clearData();
-			});
-		},
+      // text, class, function
+      vgapMap.prototype = {
+	spMenuItem : function(c, a, b) {
+	  spTools.prototype.addTool(c, a, b);
+	}
+      };
 
-		addTool : function(c, a, b) {
-			if (a == "_massClear") {
-				clearList.push(b);
-			} else {
+      this.addTool("Clear All", "clearAll", function() {
+	for (i = 0; i < clearList.length; ++i)
+	  clearList[i]();
+	spTools.prototype.clearData();
+      });
+    },
 
-				var html = $("<li class='" + a + "'>" + c + "</li>").tclick(b)
-						.appendTo("#spTools");
+    addTool : function(c, a, b) {
+      if (a == "_massClear") {
+	clearList.push(b);
+      } else {
 
-				html.mouseenter(function() {
-					var border = {
-						"border-style" : "inset",
-					}
-					html.css(border);
-				});
+	var html = $("<li class='" + a + "'>" + c + "</li>").tclick(b).appendTo("#spTools");
 
-				html.mouseleave(function() {
-					var border = {
-						"border-style" : "none",
-					}
-					html.css(border);
-				});
-			}
-		},
+	html.mouseenter(function() {
+	  var border = {
+	    "border-style" : "inset",
+	  }
+	  html.css(border);
+	});
 
-		// show hide menu when top is clicked
-		toggleMenu : function() {
-			if (showMenu) {
-				showMenu = false;
-				$("#spTools").hide();
-				$("#spToolsMenu").mousedown(function() {
-					$("#spTools").show();
-				});
-			} else {
-				showMenu = true;
-				$("#spTools").show();
-				$("#spToolsMenu").mousedown(function() {
-					$("#spTools").hide();
-				});
-			}
-		},
+	html.mouseleave(function() {
+	  var border = {
+	    "border-style" : "none",
+	  }
+	  html.css(border);
+	});
+      }
+    },
 
-		clearControls : function() {
-			showMenu = false;
-			$("#spTools").hide();
-			$("#spToolsMenu").mousedown(function() {
-				$("#spTools").show();
-			});
+    // show hide menu when top is clicked
+    toggleMenu : function() {
+      if (showMenu) {
+	showMenu = false;
+	$("#spTools").hide();
+	$("#spToolsMenu").mousedown(function() {
+	  $("#spTools").show();
+	});
+      } else {
+	showMenu = true;
+	$("#spTools").show();
+	$("#spToolsMenu").mousedown(function() {
+	  $("#spTools").hide();
+	});
+      }
+    },
 
-			this.clearData();
-		},
+    clearControls : function() {
+      showMenu = false;
+      $("#spTools").hide();
+      $("#spToolsMenu").mousedown(function() {
+	$("#spTools").show();
+      });
 
-		clearData : function() {
-			showReady = false;
-			showMenu = false;
-		},
+      this.clearData();
+    },
 
-	};
+    clearData : function() {
+      showReady = false;
+      showMenu = false;
+    },
 
-	// easy to hook in child setup here
-	var oldLoadControls = vgapMap.prototype.loadControls;
-	vgapMap.prototype.loadControls = function() {
-		oldLoadControls.apply(this, arguments);
+  };
 
-		spTools.prototype.loadControls();
-	};
+  // easy to hook in child setup here
+  var oldLoadControls = vgapMap.prototype.loadControls;
+  vgapMap.prototype.loadControls = function() {
+    oldLoadControls.apply(this, arguments);
 
-	var oldShowMap = vgaPlanets.prototype.showMap;
-	vgaPlanets.prototype.showMap = function() {
-		oldShowMap.apply(this, arguments);
-		$("#MapTip").remove();
+    spTools.prototype.loadControls();
+  };
 
-		$("#spControls").show();
-		showMenu = false;
-		$("#spToolsMenu").mousedown(function() {
-			spTools.prototype.toggleMenu();
-		});
-	};
+  var oldShowMap = vgaPlanets.prototype.showMap;
+  vgaPlanets.prototype.showMap = function() {
+    oldShowMap.apply(this, arguments);
+    $("#MapTip").remove();
+
+    $("#spControls").show();
+    showMenu = false;
+    $("#spToolsMenu").mousedown(function() {
+      spTools.prototype.toggleMenu();
+    });
+  };
 };
 
 var script = document.createElement("script");
